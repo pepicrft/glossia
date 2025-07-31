97e8c071bc2aefd0fe68eb977b8e417b023977f5
Pedro <pedro@pepicrft.me>
2025-07-31 17:36:14 +0200

    Revert to @import CSS strategy - browser caching likely the issue

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -17,6 +17,9 @@
 
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html";
+
+// Import CSS
+import "../css/app.css";
 // Establish Phoenix Socket and LiveView configuration.
 import { Socket } from "phoenix";
 import { LiveSocket } from "phoenix_live_view";
